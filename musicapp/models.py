2a2c01a8ac48d9c4e86ced74201286f39642307b
GeorgeU2030 <georgeu2021@gmail.com>
2024-05-06 12:52:27 -0500

    feat: models_has_been_created_and_migrations

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Award(models.Model):
+    description = models.TextField()
+    # 1 - Song
+    # 2 - Month
+    # 3 - Semester
+    # 4 - Year
+    type_award = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.description}"
+
+
+class Musician(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.TextField()
+    flag = models.TextField()
+    country = models.CharField(max_length=100)
+    points = models.IntegerField()
+    rating = models.FloatField()
+    best_position = models.IntegerField()
+    current_position = models.IntegerField()
+    start_date_best_position = models.DateField()
+    end_date_best_position = models.DateField()
+    points_year = models.IntegerField()
+    points_semester = models.IntegerField()
+    awards = models.ManyToManyField(Award, related_name="musicians", blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.country}"
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    gem = models.CharField(max_length=20)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    week = models.IntegerField(default=0)
+    release_year = models.PositiveSmallIntegerField()
+    genre = models.CharField(max_length=100)
+    album = models.TextField()
+    youtube = models.TextField()
+    musicians = models.ManyToManyField(Musician, related_name="songs")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Rank(models.Model):
+    week = models.IntegerField()
+    position = models.IntegerField()
+    musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.week}"
+
+
+class Ranking(models.Model):
+    period = models.CharField(max_length=50)
+    points = models.IntegerField()
+    musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Ranking {self.period}"
